ff3b60d1bc6e6575309dfeb55f22ea94ff1f187a
ajuvercr <arthur.vercruysse@ugent.be>
2026-04-28 17:48:02 +0200

    bug: fix linking issue on windows ci + add release metadata to build

--- swls/build.rs
+++ swls/build.rs
@@ -1,4 +1,11 @@
 fn main() {
+    #[cfg(target_os = "windows")]
+    {
+        println!("cargo:rustc-link-lib=advapi32");
+        println!("cargo:rustc-link-lib=userenv");
+        println!("cargo:rustc-link-lib=crypt32");
+    }
+
     vergen::EmitBuilder::builder()
         .git_sha(true)
         .git_describe(true, true, None) // includes tag if present
